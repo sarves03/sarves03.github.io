e232f40e88b7ad2d935892d22577d3f619a91d40
Sarves2003 <sarveswaran.official@gmail.com>
2025-11-09 00:37:54 +0530

    removed all files

--- src/main.server.ts
+++
@@ -1,8 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app/app';
-import { appConfig } from './app/app.config';
-
-
-const bootstrap = () => bootstrapApplication(App, appConfig);
-
-export default bootstrap;
--- src/zone-flags.ts
+++
@@ -1,1 +0,0 @@
-(window as any)['__zone_symbol__PASSIVE_EVENTS'] = ['touchstart']; // fix: Does not use passive listeners to improve scrolling performance
